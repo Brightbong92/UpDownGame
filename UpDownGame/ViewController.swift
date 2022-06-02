b4ce58206d98afb80f7dbcc7bdc2103aeff1da41
tirrilee mac mini <wkdgusqhd080@naver.com>
2022-06-02 16:09:09 +0900

    add UI 필요한 변수 생성

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,19 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var randomValue : Int = 0
+    var tryCount: Int = 0
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tryCountLabel : UILabel!
+    @IBOutlet weak var sliderValueLabel : UILabel!
+    @IBOutlet weak var minimumValueLabel : UILabel!
+    @IBOutlet weak var maximumValueLabel : UILabel!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        slider.setThumbImage(UIImage(imageLiteralResourceName: "slider_thumb"), for: .normal)
+        reset()
     }
 
     @IBAction func sliderValueChanged (_ sender: UISlider) {
         print(sender.value)
+        sliderValueLabel.text = String(Int(sender.value))
     }
     
     @IBAction func touchUpHit(_ sender: UIButton) {
         print(slider.value)
+        let hitValue : Int = Int(slider.value)
+        slider.value = Float(hitValue)
+    }
+    
+    @IBAction func toughUpResetButton(_ sender: UIButton) {
+        print("touch up reset button")
+        reset()
+    }
+    
+    func reset() {
+        print("reset")
+        randomValue = Int.random(in: 0...30)
+        tryCount = 0
+        tryCountLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minimumValueLabel.text = "0"
+        maximumValueLabel.text = "30"
+        sliderValueLabel.text = "15"
     }
 }
 
